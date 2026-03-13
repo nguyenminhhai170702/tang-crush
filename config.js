f93fe58464b1b12e4b275553069118cbc61b6fbc
nguyenminhhai170702 <85271693+nguyenminhhai170702@users.noreply.github.com>
2026-03-13 11:44:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Hà Vy à!',
+    introTitle: 'cậu à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao chị cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Mà sao cậu cứ đi nhầm đường
+    Lạc vào tim tớ lẻ loi
+    Đằng sau chữ thích đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng Cừu đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng Cừu đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 8 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    title: 'Phải chăng Chúc đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Chúc đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích tớ <33',
+    btnNo: 'Không :3',
+    question:'Trên thế giới hơn 8 tỉ người mà sao cậu lại va vào tớ <3',
+    btnReply: 'Gửi cho Minh Hải <3',
+    reply: 'thích thì thích mà không thích thì thương <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 9h anh gọi nhé Hà Vy.',
+    messDesc: 'chiều này 4h tớ qua đón Khánh Chúc đi chơi nhé.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
